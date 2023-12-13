2fe5dc7695ad0bceaaee6154c0f4a699848bbd8a
abhiram krishna <kabhiram67@gmail.com>
2023-12-14 01:01:43 +0530

    regsiterworking

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const User = require('./models/user');
 const path = require('path');
+const mysql = require('mysql2');
 
+const pool = mysql.createPool({
+  // Your MySQL database connection details
+  host:"localhost",
+  user:"root",
+  password:"",
+  database:"todo",
+});
 const app = express();
 
 app.use(express.static('public'));
@@ -50,7 +58,7 @@ app.post('/login', async (req, res) => {
 // Register route
 app.post('/register', async (req, res) => {
   const { username, email, password } = req.body;
-
+  console.log("from appjs username "+username);
   try {
     await User.registerUser(username, email, password);
     res.redirect('login');
@@ -59,15 +67,41 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
   if (!req.session.user) {
-    res.render('index');
+    res.render('login');
     return;
   }
 
   // Render main to-do page with user data
 });
 
+app.get('/check', (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      res.status(500).send('Error connecting to MySQL server');
+      console.error(err);
+    } else {
+      res.send('MySQL connection is healthy!');
+      connection.release();
+    }
+  });
+})
+
+app.post('/lol', (req, res) => {
+const userId = 2;//req.session.user.id; // Assuming there is a user id in your session
+
+  pool.query('SELECT * FROM users WHERE id = ?', [userId], (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error fetching user data from MySQL');
+    } else {
+      const userData = results[0]; // Assuming you expect one row for the user
+      console.log(userData);
+      res.render('todo', { user: userData });
+    }
+  });
+   });
 // Additional routes for to-do app functionalities
 
 app.listen(3000, () => {
--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql');
 const bcrypt = require('bcryptjs');
+const express = require('express');
+const app = express();
 
 const pool = mysql.createPool({
   // Your MySQL database connection details
@@ -8,23 +10,36 @@ const pool = mysql.createPool({
   password:"",
   database:"todo",
 });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 
 async function registerUser(username, email, password) {
+  console.log("from userjs username "+username);
   const hashedPassword = await bcrypt.hash(password, 10);
+  const userData = {
+    username: username,
+    email: email,
+    password: hashedPassword, // You should hash the password before inserting into the database
+  };
 
-  const [result] = await pool.query('INSERT INTO users SET ?', {
-    username,
-    email,
-    password: hashedPassword,
+  pool.query('INSERT INTO users SET ?', userData, (error, results, fields) => {
+    if (error) {
+      console.error('Error inserting into the database:', error);
+    } else {
+      console.log('User inserted successfully!');
+      console.log('Inserted row ID:', results.insertId);
+    }
   });
-
+  
   return result.insertId;
 }
 
 async function loginUser(username, password) {
   const [user] = await pool.query('SELECT * FROM users WHERE username = ?', [username]);
-
-  if (!user) {
+  //console.log(username);
+    if (!user) {
     throw new Error('Username not found');
   }
 
